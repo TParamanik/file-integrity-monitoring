d26ead5ca5e517cefbade4dacb1286ebdf210de5
Tanisha <tanisha.paramanik@gmail.com>
2025-04-24 10:20:51 +0530

    Changed the content of txt file, to see the changes in hash value

--- read_file.py
+++
@@ -1,4 +0,0 @@
-file = open("example.txt", "r")  # Open the file
-content = file.read()            # Read the content
-print(content)                   # Print the content
-file.close()                     # Close the file
